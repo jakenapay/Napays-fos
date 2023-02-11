81cd40bdd52cb0295e3a734cda8673ff76b34d4d
Jake Napay <jakemantesnapay@gmail.com>
2023-02-11 20:55:19 +0800

    Navbar attached

--- index.php
+++ index.php
@@ -15,11 +15,12 @@ include 'includes/config.inc.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
     <!-- CSS Files -->
-    <link rel="stylesheet" href="assets/css/navbar.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="assets/css/navbar.css?v=<?php echo time(); ?>">
 </head>
 
 <body>
+    <?php include 'navbar.php'; ?>
 
     <!-- Button trigger modal -->
     <!-- <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
@@ -27,7 +28,7 @@ include 'includes/config.inc.php';
     </button> -->
 
     <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <!-- <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -43,36 +44,15 @@ include 'includes/config.inc.php';
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 
-    <!-- Navbar -->
-    <div class="navigation-bar">
-        <div class="container bg-dark">
-            <div class="hamburger">
-                <span class="line"></span>
-                <span class="line"></span>
-                <span class="line"></span>
-            </div>
-            <div class="">
-                <img class="img-fluid" style="width: 70px;" src="napays.png">
-            </div>
-            <div class="links">
-                <ul>
-                    <li><a href="#" class="link">Home</a></li>
-                    <li><a href="#" class="link">Foods</a></li>
-                    <li><a href="#" class="link">About us</a></li>
-                    <li><a href="#" class="link">Login</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-
-
-
+    
 
 
     <!-- Bootstrap JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <!-- Navigation bar -->
+    <script src="assets/js/navbar.js"></script>
 </body>
 
 </html>
